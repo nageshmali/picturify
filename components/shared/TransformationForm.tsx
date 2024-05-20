08a152b8aad53012bbac00dc6d734482972c281f
nageshmali <malinagesh60@gmail.com>
2024-05-20 12:02:06 +0530

    done with Transformed Image User Interface

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -14,6 +14,8 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 
+import {updateCredits} from "@/lib/actions/user.actions"
+
 import { Button } from "@/components/ui/button"
 import {
     Form,
@@ -28,6 +30,7 @@ import { Input } from "@/components/ui/input"
 import { defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
 import MediaUploader from "./MediaUploader"
+import TransformedImage from "./TransformedImage"
 
 export const formSchema = z.object({
     title: z.string(),
@@ -113,7 +116,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
         setNewTransformation(null)
 
         startTransition(async () => {
-            // await updateCredits(userId, creditFee)
+            await updateCredits(userId, creditFee)
         })
     }
 
@@ -214,6 +217,15 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                         type={type}
                         />
                     )}  
+                    />
+
+                    <TransformedImage
+                    image={image}
+                    type={type}
+                    title={form.getValues().title}
+                    isTransforming={isTransforming}
+                    setIsTransforming={setIsTransforming}
+                    transformationConfig={transformationConfig}
                     />
                 </div>
 
